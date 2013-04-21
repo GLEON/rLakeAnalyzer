01a833fb1dbf126775908e866a12ad362607f86b
Luke Winslow <lawinslow@gmail.com>
2013-04-21 10:04:15 -0500

    Initial committ of uStar and waterDensity functions by Richard.

---
+++ R/uStar.R
@@ -0,0 +1,46 @@
+# wndSpeed: wind speed in m/s
+# wndHeight: height of wind measurement in m
+# averageEpiDense: average epilimnion density in kg m-3
+
+#----Author: R. Iestyn. Woolway ----
+  
+uStar <- function(wndSpeed,wndHeight,averageEpiDense){
+  
+  # define constants
+  rhoAir <- 1.2 # density of air
+  vonK <- 0.4 # von Karman constant
+
+  # -- calculate drag coefficient (Hicks, 1972)
+  if (wndSpeed < 5){
+    Cd <- 0.001
+  } else{
+    Cd <- 0.0015
+  }
+
+  # -- correct for wind measurement height if < 10 m (Amorocho and DeVries, 1980)
+  if (wndHeight != 10){
+    wndSpeed <- wndSpeed/(1-sqrt(Cd)/vonK*log(10/wndHeight))
+  }
+    
+  # -- calculate shear stress of air (Fischer et al., 1979)
+  tau <- Cd*rhoAir*wndSpeed^2
+  
+  # -- calculate uStar following Imberger (1985)
+  uStar <- sqrt(tau/averageEpiDense)
+   
+}
+
+# -- References
+#같 Hicks, B.B., 1972. A procedure for the formulation of bulk transfer
+#같 coefficients over water bodies of different sizes. Boundary-Layer
+#같 Meterology 3: 201-213
+
+#같 Amorocho, J., DeVries, J.J., 1980. A new evaluation of the wind 같
+#같 stress coefficient over water surfaces. Journal of Geophysical  같
+#같 Research 85: 433-442.
+
+#같 Fischer, H.B., List, E.J., Koh, R.C.Y., Imberger, J., Brooks, N.H.,
+#같 1979. Mixing in inland and coastal waters. Academic Press.
+
+#같 Imberger, J., 1985. The diurnal mixed layer. Limnology and Oceanography
+#같 30: 737-770.
---
+++ R/waterDensity.R
@@ -0,0 +1,79 @@
+# T is in 캜
+# S is in Pratical Salinity Scale units (dimensionless)
+# waterDensity is in grams/Liter
+
+# <<--- Effective range of function: 0-40캜, 0.5-43 Salinity--->>
+
+# -- Author: R. Iestyn. Woolway ----
+
+waterDensity <- function(wtr,S){
+  
+  # find the number of input arguments
+  nargin <- length(as.list(match.call())) -1  
+
+  # If number of input arguments is equal to 1, assume salinity is 0.
+  if (nargin == 1){
+   S <- temp*0  
+  }
+  
+  
+  # Determine which method we want to use, initially set both methods to false
+  MM = FALSE; # Martin & McCutcheon
+  UN = FALSE; # UNESCO
+
+  
+  # specify temperature and salinity range for the calculations
+  Trng <- c(0,40) # temperature range
+  Srng <- c(0.5,43) # salinity range
+  
+  
+  # check to see if all values lie within the ranges specified
+  if (isTRUE(all(S<Srng[1]))){
+    MM <- TRUE # use Martin & McCutcheon
+    } else if(!(sum(wtr<Trng[1])||sum(wtr>Trng[2])) && !(sum(S<Srng[1]))||sum(S>Srng[2])){
+      UN <- TRUE # use UNESCO
+    }
+  
+  
+  # if MM is true we use method of Martin and McCutcheon (1999)
+  if (MM){    
+    rho <- (1000*(1-(wtr+288.9414)*(wtr-3.9863)^2/(508929.2*(wtr+68.12963))))
+  } 
+  
+  # if UN is true we use method of Martin and McCutcheon (1999)
+  if (UN){  
+    # -- equation 1:
+    rho_0 <- 999.842594 + 6.793952*10^(-2)*wtr - 9.095290*10^(-3)*wtr^2 + 1.001685*10^(-4)*wtr^3 - 1.120083*10^(-6)*wtr^4 + 6.536335e-9*wtr^5
+    
+    # -- equation 2:
+    A <- 8.24493*10^(-1) - 4.0899e-3*wtr + 7.6438*10^(-5)*wtr^2 - 8.2467*10^(-7)*wtr^3 + 5.3875*10^(-9)*wtr^4
+    
+    # -- equation 3:
+    B <- -5.72466*10^(-3) + 1.0227*10^(-4)*wtr - 1.6546*10^(-6)*wtr^2
+    
+    # -- equation 4:
+    C <- 4.8314*10^(-4)
+    
+    # -- equation 5:
+    rho <- rho_0 + A*S + B*S^(3/2) + C*S  
+  } 
+  
+  # if there is a combination of fresh and saline water we need to use a combination of MM and UN
+  if (MM == FALSE && UN == FALSE){
+    rho <- wtr*0
+    for (j in 1:length(rho)){
+      rho[j] <- waterDensity(wtr[j],S[j])
+    }    
+    dim(rho) <- dim(wtr) # ensure same dimension as input array 
+  }  
+  return(rho) 
+}
+
+# -- References
+# 같 Martin, J.L., McCutcheon, S.C., 1999. Hydrodynamics and Transport 같
+# 같 for Water Quality Modeling. Lewis Publications, Boca              같
+# 같 Raton, FL, 794pp. >>
+#
+#같 Millero, F.J., Poisson, A., 1981. International one-atmosphere    같
+#같 equation of state of seawater. UNESCO Technical Papers in Marine  같
+#같 Science. No. 36. >>
