17a031d56d9709e51a9260d0c3cda73476dafb4d
Luke Winslow <lawinslow@gmail.com>
2013-07-24 15:46:24 -0500

    Changed the way thermo.depth looks for peaks. Previous versiou gave erroneous results.

--- R/thermo.depth.R
+++ R/thermo.depth.R
@@ -12,13 +12,12 @@ thermo.depth <- function(wtr, depths, Smin = 0.1, seasonal=TRUE){
   if(any(is.na(wtr))){
     return(NaN)
   }
-  
   #We need water density, not temperature to do this
   rhoVar = water.density(wtr)
   
   dRhoPerc = 0.15; #in percentage max for unique thermocline step
   numDepths = length(depths);
-  drho_dz = vector(mode="double", length=numDepths-1);
+  drho_dz = rep(NaN, numDepths-1);
   
   #Calculate the first derivative of density
   for(i in 1:numDepths-1){
@@ -30,7 +29,7 @@ thermo.depth <- function(wtr, depths, Smin = 0.1, seasonal=TRUE){
   mDrhoZ = drho_dz[thermoInd]
   thermoD = mean( depths[thermoInd:(thermoInd+1)] )
   
-  if(thermoInd > 1 && thermoInd < numDepths-1){  #if within range
+  if(thermoInd > 1 && thermoInd < (numDepths-1)){  #if within range
 		Sdn = -(depths[thermoInd+1] - depths[thermoInd])/
   		(drho_dz[thermoInd+1] - drho_dz[thermoInd])
   		
@@ -39,7 +38,8 @@ thermo.depth <- function(wtr, depths, Smin = 0.1, seasonal=TRUE){
   		
   	upD  = depths[thermoInd];
   	dnD  = depths[thermoInd+1];
-  	if( !is.infinite(Sup) & !is.infinite(Sdn) ){
+  	
+		if( !is.infinite(Sup) & !is.infinite(Sdn) ){
   		thermoD = dnD*(Sdn/(Sdn+Sup))+upD*(Sup/(Sdn+Sup));
   	}
   }
@@ -55,10 +55,10 @@ thermo.depth <- function(wtr, depths, Smin = 0.1, seasonal=TRUE){
 		mDrhoZ = pks[length(pks)]
 		SthermoInd = locs[length(pks)]
 
-		if(SthermoInd > thermoInd + 1){
+		if(SthermoInd > (thermoInd + 1)){
 			SthermoD = mean(depths[SthermoInd:(SthermoInd+1)])
 			
-			if(SthermoInd > 1 && SthermoInd < numDepths - 1){
+			if(SthermoInd > 1 && SthermoInd < (numDepths - 1)){
 				Sdn = -(depths[SthermoInd+1] - depths[SthermoInd])/
 					(drho_dz[SthermoInd+1] - drho_dz[SthermoInd])
 					
@@ -67,6 +67,7 @@ thermo.depth <- function(wtr, depths, Smin = 0.1, seasonal=TRUE){
 					
 				upD  = depths[SthermoInd]
 				dnD  = depths[SthermoInd+1]
+				
 				
 				if( !is.infinite(Sup) & !is.infinite(Sdn) ){
 					SthermoD = dnD*(Sdn/(Sdn+Sup))+upD*(Sup/(Sdn+Sup))
@@ -82,10 +83,11 @@ thermo.depth <- function(wtr, depths, Smin = 0.1, seasonal=TRUE){
 	  SthermoD = thermoD
 	  SthermoInd = thermoInd
   }
+  
   if(seasonal){
     return(SthermoD)
   }else{
-    return(thermoD)
+  	return(thermoD)
   }
   
   #list( thermoD, thermoInd, drho_dz, SthermoD, SthermoInd )
@@ -93,9 +95,32 @@ thermo.depth <- function(wtr, depths, Smin = 0.1, seasonal=TRUE){
 
 # Finds the local peaks in a vector. Checks the optionally supplied threshold 
 #  for minimum height.
-findPeaks <- function(x, thresh=0){
-	pks <- which(diff(sign(diff(x, na.pad=FALSE)),na.pad=FALSE) < 0) + 2
-	return(pks[x[pks-1] - x[pks] >= thresh])
+findPeaks <- function(dataIn, thresh=0){
+	
+	varL = length(dataIn);
+	locs = rep(FALSE, varL);
+	peaks= rep(NaN, varL);
+	
+	for(i in 2:varL-1){
+		pkI = which.max(dataIn[(i-1):(i+1)])
+		posPeak = max(dataIn[(i-1):(i+1)]);
+		
+		if(pkI == 2) {
+			peaks[i] = posPeak;
+			locs[i]  = TRUE;
+		}
+	}
+	
+	inds = 1:varL;
+	locs = inds[locs];
+	peaks= peaks[locs];
+	
+	# remove all below threshold value
+	
+	useI = peaks > thresh;
+	locs = locs[useI];
+	
+	return(locs)
 }
 
 
