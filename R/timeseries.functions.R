dc9934327746a1353c052bc790725d4543dcf8fa
Luke Winslow <lawinslow@gmail.com>
2014-01-10 11:53:00 -0600

    Added code and documentation for a Wedderburn number timeseries function.

--- R/timeseries.functions.R
+++ R/timeseries.functions.R
@@ -152,3 +152,54 @@ ts.uStar <- function(wtr, wnd, wnd.height, bathy){
 	return(output)
 }
 
+
+ts.wedderburn.number <- function(wtr, wnd, wnd.height, bathy, Ao){
+  
+  depths = get.offsets(wtr)
+  
+  # Make sure data frames match by date/time. 
+  all.data = merge(wtr, wnd, by='datetime')
+  
+  cols = ncol(all.data)
+  wtr = all.data[,-cols]
+  wnd = all.data[,c(1, cols)]
+  
+  n = nrow(wtr)
+  w.n = rep(NA, n)
+  
+  wtr.mat = as.matrix(wtr[,-1])
+  dimnames(wtr.mat) <- NULL
+  
+  for(i in 1:n){
+    #check we have all the data necessary
+    if(any(is.na(wtr.mat[i,])) || is.na(wnd[i,2])){
+      next
+    }
+    
+    m.d = meta.depths(wtr.mat[i,], depths)
+    if(any(is.na(m.d))){
+      next
+    }
+    
+    #Need epi and hypo density for wedderburn.number calc
+    epi.dens = layer.density(0, m.d[1], wtr.mat[i,], depths, bathy$areas, bathy$depths)
+    hypo.dens = layer.density(m.d[2], max(depths), wtr.mat[i,], depths, bathy$areas, bathy$depths)
+    
+    
+    uS = uStar(wnd[i,2], wnd.height, epi.dens)
+    
+    #St = schmidt.stability(wtr.mat[i,], depths, bathy$areas, bathy$depths)
+    
+    #thermo.depth <- function(wtr, depths, Smin = 0.1){\
+    #l.n[i] = lake.number(bathy$areas, bathy$depths, uS, St, m.d[1], m.d[2], hypo.dens)
+    
+    w.n[i] = wedderburn.number(hypo.dens - epi.dens, m.d[1], uS, Ao, hypo.dens)
+    
+  }
+  
+  output = data.frame(datetime=wtr$datetime, wedderburn.number=w.n)
+  
+  return(output)
+}
+
+
