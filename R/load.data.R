5f8d2d75fb4c74e75ccc6e9cfdc1e603b0fe6304
Luke Winslow <lawinslow@gmail.com>
2014-07-06 15:19:59 -0500

    Added check to load.data to try and prevent date/time issues.

--- R/load.data.R
+++ R/load.data.R
@@ -38,8 +38,15 @@ load.ts <- function(fPath, tz='GMT'){
   if( !any('datetime' %in% names(d)) ){
     stop('Timeseries file must be tab delimited and contain a column labeled \'datetime\'');
   }
+  
+  n.unique.datetime = length(unique(d$datetime))
+  
   #convert column to a real date/time format
   d$datetime = as.POSIXct(d$datetime, tz)
+  
+  if(n.unique.datetime != unique(d$datetime)){
+  	stop('Error parsing datetimes. Check your format. No duplicate datetime values allowed.')
+  }
   
   return(d)
 }
