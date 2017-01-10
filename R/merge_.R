a0ece7dee7f2eafd1ec15365eb71ba1a0b6cd9ba
Sam Albers <sam.albers@gmail.com>
2017-01-10 12:53:06 -0800

    Revert to c487b15

--- R/merge_.R
+++ R/merge_.R
@@ -3,7 +3,7 @@
 #' @description merge interval i and i+1
 #' @param i [INTEGER] interval number of the first segment to merge
 #' @param nr [INTEGER] current maximum of the interval number
-#' @param ni [INTEGER(NR)] current array with interval start point numbers
+#'@param ni [INTEGER(NR)] current array with interval start point numbers
 #' @return 'nn- [INTEGER(NR)] new array with interval start point numbers
 
 
