56e00768f57c911005f9911adc5253a45aa72bde
Doug Collinge <doug.collinge@gmail.com>
2017-01-13 15:53:46 -0800

    New version of spl.R, fixed erroneous test, now works.

--- R/spl.R
+++ R/spl.R
@@ -28,57 +28,28 @@
 #' ni = spl(ni,i=length(ni)-1)
 #' ni
 
-
-#' ## Interval too small
-#' ni =  c(10, 20)
-#' i = 2
-#'
-#' spl(ni, i)
-#'
-#' ## i too small
-#' ni =  c(10, 20, 30)
-#' i = 3
-#'
-#' spl(ni, i)
-
-
-#spl <- function(ni, i, nr) {
-#  if( i >=nr+1 ) {"i needs to be less that nr+1"}
+#spl <- function(ni, i) {
+#  if ( length(ni) > 2 ) {
+#    if ( i <= length(ni) - 1 ) {
+#      k1 = ni[i]
+#      k2 = ni[i + 1]
+#      jsplit = max(min(floor((k1 + k2) / 2), k2), k1)
+#      c(ni[1:i], jsplit, ni[(i + 1):length(ni)])
+#    } else {
+#      "i must be less length(ni)-1"
+#    }
+#  }
 #  else {
-#  k1=ni[i]
-#  k2=ni[i+1]
-#
-#  ## Feels a little silly to defining this explicitly
-#  #jsplit_cond=floor((k1+k2)/2)
-#
-#  ## Using double condition in an attempt to be defensive
-#  jsplit <- ifelse(jsplit_cond >= k2-1, k2-2,
-#                   ifelse(jsplit_cond <= k1+1, k1+2,
-#                          "Condition Not Satisfied")
-#  )
-#
-##  ## I think the following achieves the same result:
-##  jsplit <- max(min(floor((k1 + k2) / 2), k2 - 2), k1 + 2)
-#
-#  nn = ni
-#  nn[i:nr] = ni[(nr+1):(i+1)]
-#  nn[i+1] =jsplit
-#  nn
-#}
+#    "ni interval needs to be greater than 2"
+#  }
 #}
 
-spl <- function(ni, i) {
-  if ( length(ni) > 2 ) {
-    if ( i <= length(ni) - 1 ) {
-      k1 = ni[i]
-      k2 = ni[i + 1]
-      jsplit = max(min(floor((k1 + k2) / 2), k2), k1)
-      c(ni[1:i], jsplit, ni[(i + 1):length(ni)])
-    } else {
-      "i must be less length(ni)-1"
-    }
-  }
-  else {
-    "ni interval needs to be greater than 2"
-  }
+spl <- function(ni,i) {
+  stopifnot(i<length(ni))
+  k1 = ni[i]
+  k2 = ni[i+1]
+  jsplit = floor((k1+k2)/2) # Is an index, must be an integer.
+  stopifnot(k1<jsplit & jsplit<k2) 
+  c( ni[1:i], jsplit, ni[(i+1):length(ni)] )
 }
+
--- tests/testthat/test_spl.R
+++ tests/testthat/test_spl.R
@@ -1,14 +1,15 @@
 # Testing spl.R
 
-library(limnotools)
+#library(limnotools)
+source("../../R/spl.R")
 
 context("What is this, actually?")
 
 test_that("spl divides wide intervals.", {
 
-  expect_equal(spl(2,c(1,5,10,20)),c(1,5,7,10,20))
-  expect_equal(spl(1,c(1,5,10)),c(1,2,5,10))
-  expect_equal(spl(2,c(1,5,10)),c(1,5,7,10))
+  expect_equal(spl(c(1,5,10,20),2),c(1,5,7,10,20))
+  expect_equal(spl(c(1,5,10),1),c(1,3,5,10))
+  expect_equal(spl(c(1,5,10),2),c(1,5,7,10))
 
 })
 
