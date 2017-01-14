2ec15b401b2209aece832b10ecd891bc20c518e5
Doug Collinge <doug.collinge@gmail.com>
2017-01-13 16:20:02 -0800

    Added some tests.

--- tests/testthat/test_spl.R
+++ tests/testthat/test_spl.R
@@ -10,7 +10,23 @@ test_that("spl divides wide intervals.", {
   expect_equal(spl(c(1,5,10,20),2),c(1,5,7,10,20))
   expect_equal(spl(c(1,5,10),1),c(1,3,5,10))
   expect_equal(spl(c(1,5,10),2),c(1,5,7,10))
+  expect_equal(spl(c(1,2,6,10),2),c(1,2,4,6,10))
+  expect_equal(spl(c(1,2,6,10),2),c(1,2,4,6,10))
 
 })
+test_that("spl divides even,even intervals.", {
+  expect_equal(spl(c(1,2,6,10),2),c(1,2,4,6,10))
+})
 
+test_that("spl divides even,odd intervals.", {
+  expect_equal(spl(c(1,2,5,10),2),c(1,2,3,5,10))
+})
+
+test_that("spl divides odd,even intervals.", {
+  expect_equal(spl(c(1,3,6,10),2),c(1,3,4,6,10))
+})
+
+test_that("spl divides odd,odd intervals.", {
+  expect_equal(spl(c(1,3,7,10),2),c(1,3,5,7,10))
+})
 
