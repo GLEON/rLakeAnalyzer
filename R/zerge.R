1697f08f7ee878d0002e66cb3e8d53d4d01ef55d
Doug Collinge <doug.collinge@gmail.com>
2017-01-14 20:52:57 -0800

    Cleaned out cruft.

--- R/zerge.R
+++ R/zerge.R
@@ -7,11 +7,5 @@
 zerge = function(i,ni) {
   stopifnot(i<length(ni)-1) # Must have an element at n[i+2]
   stopifnot(length(ni)>2)
-#  nn = ni
-#  nn[i:nr] = ni[(i+1):(nr+1)]
-#  nn
   c( ni[1:i], ni[(i+2):length(ni)] )
-  ## Suggestion
-  ## Eliminate doubled up index entries by using unique
-  #unique(nn)
 }
