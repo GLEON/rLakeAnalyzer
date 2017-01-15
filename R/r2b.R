8927f8166714c72cdbee9fdace5eeb703b52d020
Doug Collinge <doug.collinge@gmail.com>
2017-01-14 21:37:42 -0800

    Managed to get r2b to agree with the Fortran. Also removed output parms a,b because they were never used in the original code.

--- R/r2b.R
+++ R/r2b.R
@@ -7,44 +7,34 @@
 #' @return A -[REAL] coefficient of linear regression (will zero if K1=0) (y=Ax+B), B -[REAL] coefficient of linear regression, R2B -[REAL] norm of the segment (maximum "distance" measured perpendicular to the regression line)
 #' @description  A -[REAL] coefficient of linear regression (will zero if K1=0) (y=Ax+B), B -[REAL] coefficient of linear regression, R2B -[REAL] norm of the segment (maximum "distance" measured perpendicular to the regression line)
 
-
+# Doug could not find anywhere in the original code where outputs a and b are actually used
+# he has taken then out to avoid having to return a data.frame.
 
 r2b = function(k1,k2,x,y) {
   # k1,k2: indexes of a segment of data in x,y
   # x,y: data arrays
-  # return data.frame (r2b,a,b)
+  # return r2b
 
   if(k2-k1<=2) {
     r2b=0
-    a = 0  # original code does not produce a or b?
-    b = 0
+#    a = 0  # original code does not produce a or b?
+#    b = 0
   } else {
-    #  sx=0
-    #  sxx=0
-    #  sy=0
-    #  sxy=0
-    #  eps=0
-    i = k1:(k2-1)
-    sxx = sum(x[i]^2)
-    sxy = sum(x[i]*y[i])
-    sy = sum(y[i])
-    sx = sum(x[i])
-    #  for (i in k1:k2-1){
-    #    sxx=sxx+df$x[i]^2
-    #    sxy=sxy+df$x[i]*df$y[i]
-    #    sy=sy+y[i]
-    #    sx=sx+x[i]
-    #  }
+    is = k1:(k2-1)
+    sxx = sum(x[is]^2)
+    sxy = sum(x[is]*y[k1:(k2-1)])
+    sy = sum(y[is])
+    sx = sum(x[is])
 
     n = k2-k1
-    a = 0.0
 
+    a = 0.0
     if(k1 > 1) {
       a = (n*sxy-sy*sx)/(n*sxx-sx*sx)
     }
     b = (sy-a*sx)/n
-    r2b = max( abs( y[i] - a*x[i] - b )/sqrt(a^2 + 1) )
+    r2b = max( abs( y[is] - a*x[is] - b )/sqrt(a^2 + 1) )
   }
-  data.frame(r2b=r2b,a=a,b=b)
+#  data.frame(r2b=r2b,a=a,b=b)
+  r2b
 }
-
