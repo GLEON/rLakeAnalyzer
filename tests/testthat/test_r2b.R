370ab8b06f46936b3fe474969297c4aaacdde55a
Doug Collinge <doug.collinge@gmail.com>
2017-01-15 16:34:30 -0800

    Modified test_r2b.R to make it actually a sensible test.

--- tests/testthat/test_r2b.R
+++ tests/testthat/test_r2b.R
@@ -1,13 +1,13 @@
 # Testing r2b.R
 
-source("../../R/r2b.R")
+#source("../../R/r2b.R")
 
-context("Testing the r2b() function.")
 
 x = c(0.0,1.5,2.5,4.5,5.0,6.0,7.1,9.7,10.1,12.0)
 y = c(2.0,7.0,3.0,4.0,5.0,7.0,7.1,8.3,9.1,10.0)
 
 test_that("r2b gets the right answers. Duh.", {
-  expect_equal( round(r2b(1,length(x),x,y),digits=6), 3.833333)
+  digs = 6 # number of digits to the right of decimal place we expect right.
+  expect_equal( round(r2b(1,length(x),x,y),digits=digs), round(3.83333349,digs))
 })
 
