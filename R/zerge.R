b9d45e8476053f3b5a42c329090e835817e3d927
Doug Collinge <doug.collinge@gmail.com>
2017-01-17 12:45:34 -0800

    zerge had an off-by-one error, now fixed.

--- R/zerge.R
+++ R/zerge.R
@@ -5,7 +5,8 @@
 #' @param ni [INTEGER(NR)] current array with interval start point numbers
 
 zerge = function(i,ni) {
-  stopifnot(i<length(ni)-1) # Must have an element at n[i+2]
-  stopifnot(length(ni)>2)
-  c( ni[1:i], ni[(i+2):length(ni)] )
+  stopifnot(i > 1) # Can't merge the first interval
+  stopifnot(i<length(ni)) # Must have an element at n[i+1]
+  stopifnot(length(ni)>2) # Only one interval so can't merge
+  c( ni[1:i-1], ni[(i+1):length(ni)] ) # Just removes element i
 }
