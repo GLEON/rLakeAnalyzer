f278f10114bb1679eab84ddf4bf9e4fe0cb12d93
Doug Collinge <doug.collinge@gmail.com>
2017-01-20 12:21:21 -0800

    Tests out within specified tolerance! Sure is slow, though. Density results only tested to this point.

--- tests/testthat/test_by_s_m.R
+++ tests/testthat/test_by_s_m.R
@@ -3,11 +3,16 @@ context("Testing by_s_m.")
 test_that("Run, compare results with Fortran.", {
   load("../../data/t11.rda")   # Same data as Fortran test
   # by_s_m = function(thres,z0,zmax,z,sigma)
-  resdf = by_s_m( thres=.005, z0=2.5,zmax=140, z=t11$depth, sigma=t11$density)
-  print(resdf)
+  results = by_s_m( thres=.005, z0=2.5,zmax=140, z=t11$depth, sigma=t11$density)
+  # results = list(nimax=nimax,smz=smz,sms=sms,by_s_m=ss)
+  # print(resdf)
+
 
   source("by_s_m_results.R") # From Fortran
-  print(DensityResults)
-  expect_equal( t11,t11)
+  # print(DensityResults)
+  expect_equal( results$nimax, DensityResults$nimax)
+  expect_equal( results$by_s_m, DensityResults$result)
 
+  expect_lt( sqrt(sum(results$smz-DensityResults$smz)), .00001)
+  expect_lt( sqrt(sum(results$sms-DensityResults$sms)), .00001)
 })
