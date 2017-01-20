8733445a992f8322331ce0c3f9495871872cbf15
Doug Collinge <doug.collinge@gmail.com>
2017-01-20 15:21:48 -0800

    Corrected an error in the calculation of the error (how charmingly meta!), added tests to compare results for Temperature and Salinity with the output of the Fortran code. Passes! Correct to less than 1ppm.

--- tests/testthat/test_by_s_m.R
+++ tests/testthat/test_by_s_m.R
@@ -1,18 +1,46 @@
 context("Testing by_s_m.")
 
-test_that("Run, compare results with Fortran.", {
-  load("../../data/t11.rda")   # Same data as Fortran test
+source("by_s_m_results.R") # Get results from Fortran test.
+load("../../data/t11.rda")   # Same data as Fortran test
+
+error = function(veca, vecb) {
+  sqrt(sum( (veca-vecb)^2 ))
+}
+
+maxerror = 1.e-6
+
+test_that("Run, compare density results with Fortran.", {
   # by_s_m = function(thres,z0,zmax,z,sigma)
   results = by_s_m( thres=.005, z0=2.5,zmax=140, z=t11$depth, sigma=t11$density)
   # results = list(nimax=nimax,smz=smz,sms=sms,by_s_m=ss)
-  # print(resdf)
 
+  expect_equal( results$nimax, DensityResults$nimax )
+  expect_equal( results$by_s_m, DensityResults$result )
 
-  source("by_s_m_results.R") # From Fortran
-  # print(DensityResults)
-  expect_equal( results$nimax, DensityResults$nimax)
-  expect_equal( results$by_s_m, DensityResults$result)
+  expect_lt( error(results$smz,DensityResults$smz), maxerror )
+  expect_lt( error(results$sms,DensityResults$sms), maxerror )
+})
 
-  expect_lt( sqrt(sum(results$smz-DensityResults$smz)), .00001)
-  expect_lt( sqrt(sum(results$sms-DensityResults$sms)), .00001)
+test_that("Run, compare temperature results with Fortran.", {
+  # by_s_m = function(thres,z0,zmax,z,sigma)
+  results = by_s_m( thres=.005, z0=2.5,zmax=140, z=t11$depth, sigma=t11$temper)
+  # results = list(nimax=nimax,smz=smz,sms=sms,by_s_m=ss)
+
+  expect_equal( results$nimax, TemperatureResults$nimax )
+  expect_equal( results$by_s_m, TemperatureResults$result )
+
+  expect_lt( error(results$smz,TemperatureResults$smz), maxerror )
+  expect_lt( error(results$sms,TemperatureResults$sms), maxerror )
+})
+
+test_that("Run, compare salinity results with Fortran.", {
+  # by_s_m = function(thres,z0,zmax,z,sigma)
+  results = by_s_m( thres=.005, z0=2.5,zmax=140, z=t11$depth, sigma=t11$salinity)
+  # results = list(nimax=nimax,smz=smz,sms=sms,by_s_m=ss)
+
+  expect_equal( results$nimax, SalinityResults$nimax )
+  expect_equal( results$by_s_m, SalinityResults$result )
+
+  expect_lt( error(results$smz,SalinityResults$smz), maxerror )
+  expect_lt( error(results$sms,SalinityResults$sms), maxerror )
 })
