346bec434bdf496be4e61210148e90ad7ce4850b
Sam Albers <sam.albers@gmail.com>
2017-02-09 16:50:51 -0800

    Reverted r2b to original format

--- R/r2b.R
+++ R/r2b.R
@@ -21,54 +21,25 @@
 ## Sam has replaced r2b (again)
 
 r2b = function(k1,k2,x,y) {
+  
   if(k2-k1<=2) {
     r2b=0
-    a = 0  # original code does not produce a or b?
-    b = 0
+    #    a = 0  # original code does not produce a or b?
+    #    b = 0
   } else {
     is = k1:(k2-1)
+    sxx = sum(x[is]^2)
+    sxy = sum(x[is]*y[is])
+    sy = sum(y[is])
+    sx = sum(x[is])
     
-    ## Taken from http://stackoverflow.com/a/40142643/5596534
-    ## Keeping in other parameters for future considerations
-    simplelm <- function (x, y) {
-      ## number of data
-      ## centring
-      #y0 <- .Internal(mean(y)); yc <- y - y0
-      #x0 <- .Internal(mean(x)); xc <- x - x0
-      x0 <- sum(x)/length(x); xc <- x - x0
-      y0 <- sum(y)/length(y); yc <- y - y0
-      ## fitting an intercept-free model: yc ~ xc + 0
-      xty <- drop(crossprod(xc, yc))
-      xtx <- drop(crossprod(xc))
-      slope <- xty / xtx
-      #rc <- yc - xc * slope
-      ## Pearson estimate of residual standard error
-      #sigma2 <- drop(crossprod(rc)) / (n - 2)
-      ## standard error for slope
-      #slope_se <- sqrt(sigma2 / xtx)
-      ## t-score and p-value for slope
-      #tscore <- slope / slope_se
-      #pvalue <- 2 * pt(abs(tscore), n - 2, lower.tail = FALSE)
-      ## return estimation summary for slope
-      intercept <- y0 - x0 * slope
-      return(c(slope, intercept))
-      #c("Estimate" = slope, "Std. Error" = slope_se, "t value" = tscore, "Pr(>|t|)" = pvalue)
-    }
-    
-    
-    lmResult <- simplelm(y=y[is],x=x[is])
     n = k2-k1
     
-    
+    a = 0.0
     if(k1 > 1) {
-      a = lmResult[1]
-      b = lmResult[2]
-    } else {
-      a = 0.0
-      b = (sum(y[is]) - a * sum(x[is]))/n
+      a = (n*sxy-sy*sx)/(n*sxx-sx*sx)
     }
-    
-    
+    b = (sy-a*sx)/n
     r2b = max( abs( y[is] - a*x[is] - b )/sqrt(a^2 + 1) )
   }
   
