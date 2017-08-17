79895b7982a5691245e33ceb39cb4809f63e2bef
Luke Winslow <lawinslow@gmail.com>
2017-08-17 10:22:00 -0400

    remove non-ascii characters

--- R/approx.bathy.improved.R
+++ R/approx.bathy.improved.R
@@ -4,10 +4,10 @@
 #' @description Estimates a depth-area curve for a lake using lake surface area, 
 #' maximum depth and mean depth. Two methods for estimating the curve are available;
 #' 'cone' assumes the lake is shaped as a cone and requires only surface area and 
-#' maximum depth; "voldev" uses the volume development (Vd) parameter from Håkanson (1981) 
+#' maximum depth; "voldev" uses the volume development (Vd) parameter from Hakanson (1981) 
 #' and Johansson et al. (2007). Vd is a dimensionless parameter that describes 
 #' lake basin shape in relation to the volume of cone whose base area and height 
-#' equal the surface area and maximum lake depth, it is estimated as Vd = Zmean/Zmax (Håkanson et al. 2000). 
+#' equal the surface area and maximum lake depth, it is estimated as Vd = Zmean/Zmax (Hakanson et al. 2000). 
 #' Method "voldev' requires lake surface area, mean and maximum depth. Depths at 
 #' which the area is estimated can be set by as a numeric vector or as a regularly spaced sequence.
 #' 
@@ -21,8 +21,8 @@
 #' 
 #' @return a dataframe which defines the lake area for each depth. Columns are depths (m) and Area.at.z (m^2). Area at 0 m should equal the user entered lkeArea.
 #' 
-#' @references Håkanson, L. (1981). On lake bottom dynamics – the energy– topography factor. Canadian Journal of Earth Sciences, 18, 899–909.
-#' Johansson, H., A. A. Brolin, and L. Håkanson. 2007. New approaches to the modelling of lake basin morphometry. Environ. Model. Assess. 12: 213–228.
+#' @references Hakanson, L. (1981). On lake bottom dynamics - the energy- topography factor. Canadian Journal of Earth Sciences, 18, 899-909.
+#' Johansson, H., A. A. Brolin, and L. Hakanson. 2007. New approaches to the modelling of lake basin morphometry. Environ. Model. Assess. 12: 213-228.
 #'
 #' @examples  
 #' Voldev.ex = approx.bathy(Zmax = 25, Zmean = 12, lkeArea = 39400000, method = "voldev")
@@ -50,7 +50,7 @@ approx.bathy <- function(Zmax, lkeArea, Zmean = NULL, method = "cone", zinterval
     }
       Zrel = depths/Zmax
       Vd = 3*(Zmean/Zmax) 
-      fVd = (1.7/Vd) + 2.5 - 2.4*Vd +0.23*Vd^3 # derived from Håkanson, L. (1981) as cited in Johansson et al. 2007
+      fVd = (1.7/Vd) + 2.5 - 2.4*Vd +0.23*Vd^3 # derived from Hakanson, L. (1981) as cited in Johansson et al. 2007
       for (i in 1:length(Zrel)){
         Area.at.z = lkeArea*( (1-Zrel[i]) * (1 + Zrel[i]*sin(sqrt(Zrel[i])) ) )^fVd
         tmp = data.frame(depths = depths[i], Area.at.z = Area.at.z)
