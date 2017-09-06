d3630c0f39cbdc3de621f66acc7a9ecdc985ea6d
boshek <sam.albers@gmail.com>
2017-09-05 21:33:48 -0700

    Only keeping latesummer data

--- R/data.R
+++ R/data.R
@@ -1,17 +1,3 @@
-#' Early Spring Profile
-#'
-#' \describe{
-#'   \item{depth}{depth}
-#'   \item{temper}{temperature}
-#'   \item{salinity}{salinity}
-#'   \item{oxygen}{oxygen}
-#'   \item{oxygen.sat}{oxygen saturation}
-#'   \item{density}{Density}
-#'   ...
-#' }
-#' @source Supplied 
-"earlyspring"
-
 #' Late Summer Profile
 #'
 #' \describe{
@@ -23,36 +9,4 @@
 #'   \item{density}{Density}
 #'   ...
 #' }
-#' @source Supplied 
 "latesummer"
-
-#' Late Fall Profile
-#'
-#' \describe{
-#'   \item{depth}{depth}
-#'   \item{temper}{temperature}
-#'   \item{salinity}{salinity}
-#'   \item{oxygen}{oxygen}
-#'   \item{oxygen.sat}{oxygen saturation}
-#'   ...
-#' }
-#' @source Supplied 
-"latefall"
-
-
-#' 97130187.t11
-#'
-#' Data supplied with original FORTRAN code
-#'
-#' @format A data frame with 401 rows and 6 variables:
-#' \describe{
-#'   \item{depth}{depth}
-#'   \item{temper}{temperature}
-#'   \item{salinity}{salinity}
-#'   \item{oxygen}{oxygen}
-#'   \item{oxygen.sat}{oxygen saturation}
-#'   \item{density}{Density}
-#'   ...
-#' }
-#' @source Supplied by R Thomson
-"t11"
