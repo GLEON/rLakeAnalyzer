6d5cd295f15bb4c3a1ccadc01d5609e46a0ef56d
boshek <sam.albers@gmail.com>
2017-09-19 21:33:26 -0700

    adding top-level documentation file

---
+++ R/rLakeAnalyzer.R
@@ -0,0 +1,14 @@
+#' Lake Physics Tools
+#' 
+#' Standardized methods for calculating common important derived 
+#' physical features of lakes including water density based based on
+#' temperature, thermal layers, thermocline depth, lake number, Wedderburn
+#' number, Schmidt stability and others.
+#' @docType package
+#' @name tidyhydat
+#' 
+#' @importFrom grDevices colorRampPalette
+#' @importFrom graphics abline axis filled.contour legend lines plot segments title
+#' @importFrom stats approx
+#' @importFrom utils read.table
+NULL
